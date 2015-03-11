a3ad167260e1b8a594be3eabd8bef37e10bcd20d
Piotr Szulc <szulo123@gmail.com>
2015-03-11 21:54:42 +0100

    Rotating player sprite

---
+++ Asteroids/Game1.cs
@@ -0,0 +1,102 @@
+﻿#region Using Statements
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.GamerServices;
+#endregion
+
+namespace Asteroids
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        Player player;
+
+        public Game1() : base()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            player = new Player(new Vector2(30, 30));
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            var playerSprite = Content.Load<Texture2D>(@"player");
+            Player.Sprite = playerSprite;
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Left))
+            {
+                player.RotateLeft();
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Right))
+            {
+                player.RotateRight();
+            }
+
+            player.Update();
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Black);
+
+            spriteBatch.Begin();
+            player.Draw(spriteBatch);
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Asteroids/Player.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Asteroids
+{
+    class Player
+    {
+        const float ROTATE_SPEED = 0.05f;
+
+        public static Texture2D Sprite;
+
+        public Vector2 Position { get; private set; }
+        public Vector2 Velocity { get; private set; }
+        private float angle = 0.0f;
+        public float Angle {
+            get { return angle; }
+            private set { this.angle = value % (2 * MathHelper.Pi); }
+        }
+        private Vector2 RotationOrigin { get; set; }
+
+        public Player(Vector2 position)
+        {
+            Position = position;
+            Velocity = new Vector2(0.0f, 0.0f);
+            Angle = MathHelper.Pi;
+            RotationOrigin = new Vector2(10, 15);
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(Sprite, Position, null, Color.White, Angle, RotationOrigin, 1.0f, SpriteEffects.None, 0f);
+        }
+
+        public void RotateLeft()
+        {
+            Angle -= ROTATE_SPEED;
+        }
+
+        public void RotateRight()
+        {
+            Angle += ROTATE_SPEED;
+        }
+
+        public void Update()
+        {
+            Position = Position + Velocity;
+        }
+    }
+}
