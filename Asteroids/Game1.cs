d62d9b5198788c3b9527a544a03b0057c2927383
Piotr Szulc <szulo123@gmail.com>
2015-08-05 21:17:41 +0200

    Moved project to MonoGame 3.4

--- Asteroids/Game1.cs
+++ Asteroids/Game1.cs
@@ -2,11 +2,8 @@
 using System;
 using System.Collections.Generic;
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
-using Microsoft.Xna.Framework.Storage;
-using Microsoft.Xna.Framework.GamerServices;
 using Asteroids.View;
 #endregion
 
